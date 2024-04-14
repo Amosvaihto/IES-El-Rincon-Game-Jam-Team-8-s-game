191bacfc84d3064957e767f510fe941c14f59ac6
l3xfrant3s <l3xfrant3s@outlook.com>
2024-04-14 17:08:51 +0100

    Update GameUtils.cs

--- Assets/Scripts/GameUtils.cs
+++ Assets/Scripts/GameUtils.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public static class GameUtils
 {
 
     public static float basePositionX = -1f;
 
+    public static void UpdateSliderBar(Slider slider, float currentValue, float maxValue){
+        slider.value = currentValue / maxValue;
+    }
 }
+
