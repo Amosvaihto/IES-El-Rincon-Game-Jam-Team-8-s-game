9474174bd50333dc1c715ea0c14993bed90225c7
Carlos Isaac Caraballo Casañas <carlosicaraballo1999@gmail.com>
2024-04-14 15:01:52 +0200

    New logic of enemy generator

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -19,11 +19,11 @@ public class EnemyGenerator : MonoBehaviour
     private float timerRound = 0f;
     private List<EnemyListStruc> enemies = new List<EnemyListStruc>();
     private int currentRound = 1;
-    private float[] differentsRespawnTime = { 10f, 9f, 8f, 7f, 6f };
-    private int respawnTimeIndex = 0;
+    private float minRespawnTime = 3f;
+    private float respawnTime = 15f;
 
     public List<EnemyListStruc> enemiesLVL1 = new List<EnemyListStruc>();
-    public float roundTime = 30f;
+    public float roundTime = 20f;
     public TextMeshProUGUI textRound;
 
     void Start()
@@ -31,7 +31,6 @@ public class EnemyGenerator : MonoBehaviour
         this.enemies.AddRange(enemiesLVL1);
         InstanciateRandomEnemyList();
         textRound.text = "Ronda: " + this.currentRound;
-        respawnTimeIndex = 0;
     }
 
     // Update is called once per frame
@@ -39,25 +38,21 @@ public class EnemyGenerator : MonoBehaviour
     {
         this.timerSpawn += Time.deltaTime;
         this.timerRound += Time.deltaTime;
-        if (timerSpawn > differentsRespawnTime[respawnTimeIndex])
+        if (timerSpawn > respawnTime)
         {
-            timerSpawn -= differentsRespawnTime[respawnTimeIndex]; //This is so it doesn't lose a few miliseconds every spawn
+            timerSpawn -= respawnTime;
             InstanciateRandomEnemyList();
         }
         if (timerRound > roundTime)
         {
-            if (currentRound % 5 == 0)
-            {
-                respawnTimeIndex = 0;
-            }
-            else
-            {
-                respawnTimeIndex += 1;
-            }
             timerRound -= roundTime;
             this.currentRound += 1;
             textRound.text = "Ronda: " + this.currentRound;
-            Debug.Log(differentsRespawnTime[respawnTimeIndex]);
+            respawnTime -= 0.5f;
+            if (respawnTime < minRespawnTime)
+            {
+                respawnTime = minRespawnTime;
+            }
         }
     }
 
